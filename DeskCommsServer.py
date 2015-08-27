51de1f8bc144b7fde8edf20935306c1d845c8476
Evan <evilGoldfish@users.noreply.github.com>
2015-08-27 22:29:06 +1000

    Added debug mode to server and made handshake request smaller

--- DeskCommsServer.py
+++ DeskCommsServer.py
@@ -5,15 +5,20 @@ import sys
 from time import sleep
 
 arduino = None
+debugMode = 0
 try:
 	print("Connecting to Arduino...")
 	arduino = serial.Serial("COM3", 9600)
 except serial.serialutil.SerialException:
-	sys.exit("Error: Could not connect to Arduino.")
+	if (debugMode):
+		print("Error: Could not connect to Arduino. Debug mode is enabled, so continuing...")
+	else:
+		sys.exit("Error: Could not connect to Arduino.")
 
-sleep(2)
-arduino.write(b'\x99\x61')
-print("Connected to Arduino.")
+if (arduino):
+	sleep(2)
+	arduino.write(b'\x99\x61')
+	print("Connected to Arduino.")
 print("Switches not working? Make sure that your desk has MIDI enabled.")
 print("WARNING: LSC maXim desks have a bug that causes a system reset on the desk if MIDI is enabled when turning it off. Remember to turn MIDI off before you turn off the desk!")
 
@@ -32,9 +37,12 @@ print("Server started on port {0}".format(str(UDP_PORT)));
 
 while True:
 	data, addr = desksocket.recvfrom(1024);
-	if data == bytes("handshake","utf-8"):
-		print("A client has sent a connection request, performing handshake...")
-		handshake(addr)
-		print("Handshake complete.");
-	elif data[0] == 0x90 or data[0] == 0x80:
-		arduino.write(data)
+	if len(data) > 0:
+		if data[0] == 0x42:
+			print("A client has sent a connection request, performing handshake...")
+			handshake(addr)
+			print("Handshake complete.");
+		elif data[0] == 0x90 or data[0] == 0x80:
+			arduino.write(data)
+		else:
+			print(data)

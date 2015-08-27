bb08f57a611fdf58b4fcb7f25bfe7c16875312c7
Evan <evilgoldfish01@me.com>
2015-08-27 17:22:02 +1000

    Added socket server in Python

---
+++ DeskCommsServer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import socket
+import serial
+import sys
+from time import sleep
+
+arduino = None
+try:
+	print("Connecting to Arduino...")
+	arduino = serial.Serial("COM3", 9600)
+except serial.serialutil.SerialException:
+	sys.exit("Error: Could not connect to Arduino.")
+
+sleep(2)
+arduino.write(b'\x99\x61')
+print("Connected to Arduino.")
+
+def handshake(address):
+	if arduino is None:
+		desksocket.sendto(b'\x00',addr)
+	else:
+		desksocket.sendto(b'\xCC',addr)
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 41585
+
+desksocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
+desksocket.bind((UDP_IP, UDP_PORT));
+print("Server started on port {0}".format(str(UDP_PORT)));
+
+while True:
+	data, addr = desksocket.recvfrom(1024);
+	if data == bytes("handshake","utf-8"):
+		print("A client has sent a connection request, performing handshake...")
+		handshake(addr)
+		print("Handshake complete.");
+	elif data[0] == 0x90 or data[0] == 0x80:
+		arduino.write(data)
